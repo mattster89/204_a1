af318e803a23bad6d94cd310b1f50a6f5c4a1bd4
mattster89 <matthewlansdaal12@gmail.com>
2020-07-27 00:32:29 +1200

    Update simpleserver.java

--- simpleserver.java
+++ simpleserver.java
@@ -6,23 +6,23 @@ import java.net.Socket;
 public class simpleserver{
 	public static void main(String[] args) {
 		try{
-			ServerSocket ss = new ServerSocket(0);
-			System.out.println(ss.getLocalPort());
+			ServerSocket ss = new ServerSocket(0); //set the port to the first available
+			System.out.println(ss.getLocalPort()); //print the port so it can be put into the client arguments
 			while(true) {
 				Socket client = ss.accept(); // Blocking
-				System.out.println("Obtained connection");
-				InetAddress clientName = client.getLocalAddress();
-				String hostName = clientName.getHostName();
-				String hostAddress = clientName.getHostAddress();
-				System.out.println(hostAddress + "\n" + hostName);
+				System.out.println("Obtained connection"); //display when a connection has been made
+				InetAddress clientName = client.getLocalAddress(); //get the ip of the connecting client
+				String hostName = clientName.getHostName(); //set string for printing
+				String hostAddress = clientName.getHostAddress(); //set string for printing
+				System.out.println(hostAddress + "\n" + hostName); //print to the client
 				
-				BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
-				writer.write("Hello, " + hostName + ".\nYour IP address is " + hostAddress);
+				BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(client.getOutputStream())); //set up writer
+				writer.write("Hello, " + hostName + ".\nYour IP address is " + hostAddress); //write to client
 				writer.flush();
 				client.close();
 			}
 		}
-		catch(Exception e) {
+		catch(Exception e) { //error catching
 			System.err.println("There has been an error");
 			
 		}
