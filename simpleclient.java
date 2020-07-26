3647f435883e949fc19dd0aaa952927fb941e69c
mattster89 <matthewlansdaal12@gmail.com>
2020-07-27 00:32:27 +1200

    Update simpleclient.java

--- simpleclient.java
+++ simpleclient.java
@@ -9,16 +9,16 @@ public class simpleclient{
 		try{
 			String hostName = (args[0]); //sets host name to inputted address
 			InetAddress echoServerIP = InetAddress.getByName(hostName);
-			Socket clientSocket = new Socket(echoServerIP, Integer.parseInt(args[1]));
+			Socket clientSocket = new Socket(echoServerIP, Integer.parseInt(args[1])); //set the socket to the second given argument (the port)
 			
-			InputStreamReader in = new InputStreamReader(clientSocket.getInputStream());
-			BufferedReader reader = new BufferedReader(in);
+			InputStreamReader in = new InputStreamReader(clientSocket.getInputStream()); //initialise the reader
+			BufferedReader reader = new BufferedReader(in); //initialise the reader
 			String line;
-			while ((line = reader.readLine()) != null){
+			while ((line = reader.readLine()) != null){ //Print everything in the recieved reader string
 				System.out.println(line);
 			}
 		}
-		catch(Exception e) {
+		catch(Exception e) { //error catching
 			System.err.println("There has been an error");
 			
 		}
